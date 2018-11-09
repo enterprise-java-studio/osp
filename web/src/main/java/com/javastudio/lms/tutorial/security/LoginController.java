94516586ae0464048742b4d3c7b287c3a5947116
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-11-09 12:59:50 +0330

    Try to authenticate user

--- web/src/main/java/com/javastudio/lms/tutorial/security/LoginController.java
+++ web/src/main/java/com/javastudio/lms/tutorial/security/LoginController.java
@@ -1,8 +1,14 @@
 package com.javastudio.lms.tutorial.security;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -49,5 +55,20 @@ public class LoginController implements Serializable {
         logger.info(username);
         logger.info(password);
         logger.info(String.valueOf(rememberMe));
+
+        Subject currentUser = SecurityUtils.getSubject();
+        boolean justLogged = !currentUser.isAuthenticated();
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+
+        try {
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            token.setRememberMe(rememberMe);
+            currentUser.login(token);
+            logger.info("User {} has logged in successfully.", token.getUsername());
+        } catch (RuntimeException e) {
+            logger.error("Unknown user, please try again", e);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), ""));
+        }
     }
 }
