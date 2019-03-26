3e207155774090178f32c5c275139e4fa63090f0
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2019-03-26 14:18:57 +0430

    logback in web

--- web/src/main/java/com/javastudio/lms/tutorial/web/controller/user/UserManager.java
+++ web/src/main/java/com/javastudio/lms/tutorial/web/controller/user/UserManager.java
@@ -4,6 +4,8 @@ import com.javastudio.lms.tutorial.web.controller.base.ManagerBase;
 import com.javastudio.tutorial.api.GeneralServiceApi;
 import com.javastudio.tutorial.api.UserService;
 import com.javastudio.tutorial.dto.UserDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
@@ -11,17 +13,20 @@ import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
 
-@ViewScoped
 @Named
+@ViewScoped
 public class UserManager extends ManagerBase<UserDTO> implements Serializable {
 
     private static final long serialVersionUID = -9051371651827827993L;
+
+    Logger logger = LoggerFactory.getLogger(UserManager.class);
 
     @EJB
     UserService service;
 
     public UserManager() {
         super(UserDTO.class);
+        logger.info("UserManager constructor has called.");
     }
 
     @Override
